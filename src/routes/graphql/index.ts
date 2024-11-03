ce843f18722dfe274b187877c5659e92bcc99c1a
Yana <annetart1994@gmail.com>
2024-11-03 20:21:30 +0200

    feat: limit the complexity of the graphql queries by their depth with graphql-depth-limit package

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, validate, parse, GraphQLError } from 'graphql';
 import getSchema from './get-schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -17,18 +18,37 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
-      const { data, errors } = await graphql({
-        schema: getSchema,
-        source: query,
-        variableValues: variables,
-        contextValue: { prisma },
-      });
 
-      return { data, errors };
+      try {
+        const parsedQuery = parse(query);
+        const validationErrors = validate(getSchema, parsedQuery, [depthLimit(5)]);
+
+        if (validationErrors.length > 0) {
+          console.error('GraphQL Validation Errors:', validationErrors);
+          return { errors: validationErrors };
+        }
+
+        const { data, errors } = await graphql({
+          schema: getSchema,
+          source: query,
+          variableValues: variables,
+          contextValue: {
+            prisma,
+          },
+        });
+
+        return { data, errors };
+      } catch (error) {
+        if (error instanceof GraphQLError) {
+          console.error('GraphQL Error:', error.message);
+          return { errors: [error] };
+        } else {
+          console.error('Unknown Error:', error);
+          return { errors: [new GraphQLError('Internal server error')] };
+        }
+      }
     },
   });
 };
 
 export default plugin;
-
-
