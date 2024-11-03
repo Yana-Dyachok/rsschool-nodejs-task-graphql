3d8f9dc92e3ccf44798f5c67f1548790b11b2ad9
Yana <annetart1994@gmail.com>
2024-11-03 20:26:26 +0200

    fix: remove unnecessary code

--- src/routes/graphql/components/profile/profile-type.ts
+++ src/routes/graphql/components/profile/profile-type.ts
@@ -1,38 +1,3 @@
-// import { GraphQLBoolean, GraphQLInt, GraphQLObjectType } from 'graphql';
-// import { Profile } from '@prisma/client';
-// import { UUIDType } from '../../types/uuid.js';
-// import { IContext } from '../../types/interfaces/context.js';
-// import { MemberType } from '../member-type/member-type.js';
-// import { memberTypeId } from '../../types/member-type-id.js';
-// import { UserType } from '../users.ts/user-type.js';
-
-// const resolveUser = async ({ userId }: Profile, _: unknown, { prisma }: IContext) =>
-//   prisma.user.findUnique({ where: { id: userId } });
-
-// const resolveMemberType = async (
-//   { memberTypeId }: Profile,
-//   _: unknown,
-//   { loaders, prisma }: IContext,
-// ) => {
-//   if (loaders.memberLoader) {
-//     return loaders.memberLoader.load(memberTypeId);
-//   }
-//   return prisma.memberType.findUnique({ where: { id: memberTypeId } });
-// };
-
-// export const ProfileType = new GraphQLObjectType({
-//   name: 'Profile',
-//   fields: () => ({
-//     id: { type: UUIDType },
-//     isMale: { type: GraphQLBoolean },
-//     yearOfBirth: { type: GraphQLInt },
-//     userId: { type: UUIDType },
-//     memberTypeId: { type: memberTypeId },
-//     user: { type: UserType, resolve: resolveUser },
-//     memberType: { type: MemberType, resolve: resolveMemberType },
-//   }),
-// });
-
 import { GraphQLBoolean, GraphQLInt, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import { UUIDType } from '../../types/uuid.js';
 import { MemberType } from '../member-type/member-type.js';
