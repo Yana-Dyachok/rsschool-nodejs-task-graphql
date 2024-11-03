1ad66680a30c37d9417524a9ff1fe8c709820a32
Yana <annetart1994@gmail.com>
2024-11-03 11:56:28 +0200

    feat: add user  type and query, check tests

---
+++ src/routes/graphql/components/users/user-type.ts
@@ -0,0 +1,59 @@
+import { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLFloat, GraphQLList } from "graphql";
+import { PrismaClient } from "@prisma/client";
+import { UUIDType } from "../../types/uuid.js";
+import { PostType } from "../post/post-type.js";
+import { ProfileType } from "../profile/profile-type.js";
+import { IContext } from "../../types/interfaces/context.js";
+
+export const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: {
+      type: ProfileType,
+      resolve: async (parent: { id: string }, _: unknown, context: IContext) => {
+        const profile = await context.prisma.profile.findUnique({
+          where: {
+            userId: parent.id,
+          },
+        });
+        return profile;
+      },
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async (parent: { id: string }, _: unknown, context: IContext) => {
+        const posts = await context.prisma.post.findMany({
+          where: {
+            authorId: parent.id,
+          },
+        });
+        return posts;
+      },
+    },
+    userSubscribedTo: {
+      type: new GraphQLList(UserType),
+      resolve: async (parent: { id: string }, _: unknown, context: IContext) => {
+        const subscriptions = await context.prisma.user.findMany({
+          where: {
+            subscribedToUser: { some: { subscriberId: parent.id } }
+          }
+        });
+        return subscriptions;   
+      },
+    },
+    subscribedToUser: {
+      type: new GraphQLList(UserType),
+      resolve: async (parent: { id: string }, _: unknown, context: IContext) => {
+        const subscribers = await context.prisma.user.findMany({
+          where: {
+            userSubscribedTo: { some: { authorId: parent.id } }
+          }
+        });
+        return subscribers;
+      },
+    }
+  }),
+});
--- src/routes/graphql/get-schema.ts
+++ src/routes/graphql/get-schema.ts
@@ -2,12 +2,15 @@ import { GraphQLObjectType, GraphQLSchema } from 'graphql';
 import { getMemberTypeQuery } from './components/member-type/get-member-type-query.js';
 import { getPostQuery } from './components/post/get-post-query.js';
 import { getProfileQuery } from './components/profile/get-profile-query.js';
+import { getUserQuery } from './components/users/get-user-query.js';
+
 const query = new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
     ...getMemberTypeQuery,
     ...getPostQuery,
     ...getProfileQuery,
+    ...getUserQuery,
   }),
 });
 
